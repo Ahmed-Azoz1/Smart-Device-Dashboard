7de93e4d42009666538ece65d731558ead43d888
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2025-04-06 13:02:19 +0200

    Set up routing with protected and public routes for login, signup, and home pages

--- iot-frontend/src/App.jsx
+++ iot-frontend/src/App.jsx
@@ -1,11 +1,52 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import DeviceDetailsPage from './pages/DeviceDetailsPage';
+import Header from './components/Layout/Header';
+import SignUpPage from './pages/SignUpPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <Router>
+      <Header />
+      <Routes>
+        {/* صفحات غير مسموح الوصول لها لو فيه توكن */}
+        <Route
+          path="/login" element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup" element={
+            <PublicRoute>
+              <SignUpPage />
+            </PublicRoute>
+          }
+        />
 
-export default App
+        {/* الصفحات المحمية */}
+        <Route
+          path="/" element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/device/:id" element={
+            <ProtectedRoute>
+              <DeviceDetailsPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
